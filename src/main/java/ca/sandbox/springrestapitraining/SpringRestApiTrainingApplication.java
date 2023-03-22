b5be34fa94eb0a79cf0950f7b0d41991de208679
vgalerne <valentin.galerne@gmail.com>
2023-03-22 15:36:02 -0400

    adding spring initializr files

---
+++ src/main/java/ca/sandbox/springrestapitraining/SpringRestApiTrainingApplication.java
@@ -0,0 +1,13 @@
+package ca.sandbox.springrestapitraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sandbox/springrestapitraining/SpringRestApiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sandbox.springrestapitraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
